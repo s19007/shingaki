9ed3be4e77c7f4351f95954d89041b86d1f3bf2f
shingaki <s19007@std.it-college.ac.jp>
2019-06-06 20:36:14 +0900

    0604

---
+++ 0604/060404.sh
@@ -0,0 +1,13 @@
+if [ -d "$1" ]; then
+    # ディレクトリないのファイルが実行可能ファイルかチェックして
+    # 実行可能ファイルであれば表示
+    for file in $(find "$1" -maxdepth 1 -type f)
+    do
+        # fileが実行ファイルであれば表示
+        if [ -x "$file"]; then
+            echo $(basename "$file")
+        fi
+    done
+else
+    echo "${1}:ディレクトリではありません"
+fi
